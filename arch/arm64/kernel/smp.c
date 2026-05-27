d4067abbadb1273556bba8755963b463196c4a8a
abu-adm <github@toruk.cz>
2026-05-28 00:22:43 +0200

    defconfig: redbull: disable QCOM_QHEE_ENABLE_MEM_PROTECTION

--- arch/arm64/kernel/smp.c
+++ arch/arm64/kernel/smp.c
@@ -413,8 +413,7 @@ static void __init hyp_mode_check(void)
 		pr_info("CPU: All CPU(s) started at EL1\n");
 }
 
-#if defined(CONFIG_QCOM_SCM_MODULE) && \
-	defined(CONFIG_QCOM_QHEE_ENABLE_MEM_PROTECTION)
+#if !defined(CONFIG_QCOM_QHEE_ENABLE_MEM_PROTECTION)
 int __init __weak scm_enable_mem_protection(void)
 {
 	return 0;
